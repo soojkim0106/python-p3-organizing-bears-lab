7a9f33ac6792892c30c65edaa4dfb52c31c55775
Soojungkim <soojung.kimm@gmail.com>
2024-03-13 21:14:36 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr.Chocolate', 10, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2,'Rowdy', 9, 'M', 'Brown', 'Rowdy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3,'Tabitha', 20, 'F', 'Brown', 'Adventurous', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 25, 'M', 'Brown', 'Wild', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 4, 'F', 'Brown', 'Petty', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6,'Grinch', 2, 'M', 'Brown', 'Grincy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7,'Wendy', 8, 'F','Brown', 'Smiley', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 49, 'M', 'Brown', 'Scary', 0);
